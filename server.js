447a352c5fe48ade32c2dc41a48422d0daacafb2
ferrnd <sombarepam@gmail.com>
2025-09-22 15:28:10 -0300

    Três filtros adicionados.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import pacientesRoutes from "./src/routes/pacientesRoutes.js"
+import dados from "./src/models/dados.js";
 
+const { pacientes } = dados;
 const serverPort = process.env.PORT || 3000;
 const app = express();
 
@@ -12,6 +14,57 @@ app.get("/", (req, res) => {
   res.send("🚀 Servidor funcionando...");
 });
 
+app.get("/pacientes/convenio/:convenio", (req, res) => {
+  let convenio = req.params.convenio.toLocaleLowerCase();
+  const paciente = pacientes.filter((p) =>
+p.convenio.toLocaleLowerCase().includes(convenio)
+  );
+  if (pacientes) {
+    res.status(200).json(paciente);
+  }
+  res.status(404)({
+    error: "paciente não encontrado.",
+  });
+});
+
+app.get("/pacientes/endereco/:endereco", (req, res) => {
+  let endereco = req.params.endereco.toLocaleLowerCase();
+  const paciente = pacientes.filter((p) =>
+p.endereco.toLocaleLowerCase().includes(endereco)
+  );
+  if (pacientes) {
+    res.status(200).json(paciente);
+  }
+  res.status(404)({
+    error: "paciente não encontrado.",
+  });
+});
+
+app.get("/pacientes/historico/:historico", (req, res) => {
+  let historico = req.params.historico.toLocaleLowerCase();
+  const paciente = pacientes.filter((p) =>
+p.historico.toLocaleLowerCase().includes(historico)
+  );
+  if (pacientes) {
+    res.status(200).json(paciente);
+  }
+  res.status(404)({
+    error: "paciente não encontrado.",
+  });
+});
+
+app.get("/pacientes/idade/crianca/:dataNascimento", (req, res) => {
+  let dataNascimento = req.params.dataNascimento.toLocaleLowerCase();
+  const paciente = pacientes.filter((p) =>
+p.dataNascimento.toLocaleLowerCase().includes(dataNascimento)
+  );
+  if (pacientes) {
+    res.status(200).json(paciente);
+  }
+  res.status(404)({
+    error: "paciente não encontrado.",
+  });
+});
 
 app.use("/pacientes", pacientesRoutes);
 
